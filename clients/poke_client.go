9a9e52b8c35121a620b507317d26217497ec7bd3
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-08-23 14:52:17 -0400

    docs: polish up godocs.

--- clients/poke_client.go
+++ clients/poke_client.go
@@ -28,48 +28,42 @@ func NewPokeClient(l *slog.Logger, bu string, hc *http.Client) *PokeClient {
 	}
 }
 
-// GetPokemonByName invokes the Pokemon API to get a Pokemon by the provided name
-// and return the Pokemon metadata in a custom Pokemon struct or an error object if an unexpected failure occurs.
-func (pc *PokeClient) GetPokemonByName(id string) (*models.PokemonResponse, error) {
-	path, err := url.JoinPath(pc.baseUrl, id)
+// GetPokemonByName invokes the pokemon API to get a pokemon with the provided name
+func (pc *PokeClient) GetPokemonByName(name string) (*models.PokemonResponse, error) {
+	path, err := url.JoinPath(pc.baseUrl, name)
 	if err != nil {
-		return nil, fmt.Errorf("%w: error creating URL to call pokemon API: %v", errors.ErrInternalServerError, err)
+		return nil, fmt.Errorf(
+			"%w: error creating URL to call pokemon API: %v", errors.ErrInternalServerError, err)
 	}
 
 	res, err := pc.client.Get(path)
-
 	if err != nil {
-		return nil, fmt.Errorf("%w: too many redirects or HTTP protocol error: %v", errors.ErrInternalServerError, err)
+		return nil, fmt.Errorf(
+			"%w: too many redirects or HTTP protocol error: %v", errors.ErrInternalServerError, err)
 	}
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("http request failed: %w", errors.ErrNotFound)
+		return nil, fmt.Errorf(
+			"http request failed: %w", errors.ErrNotFound)
 	}
-
 	defer res.Body.Close()
+
 	body, err := io.ReadAll(res.Body)
-
 	if err != nil {
-		return nil, fmt.Errorf("%w: error reading response body: %v", errors.ErrInternalServerError, err)
+		return nil, fmt.Errorf(
+			"%w: error reading response body: %v", errors.ErrInternalServerError, err)
 	}
 
-	pokeRes, err := pc.unmarshalPokemon(body)
-
+	var pr models.PokemonResponse
+	err = json.Unmarshal(body, &pr)
 	if err != nil {
-		return nil, fmt.Errorf("%w: error deserializing response body: %v", errors.ErrInternalServerError, err)
+		return nil, fmt.Errorf(
+			"%w: error deserializing response body: %v", errors.ErrInternalServerError, err)
 	}
-
-	return pokeRes, nil
+	return &pr, nil
 }
 
-// unmarshalPokemon takes a list of bytes representing a Pokemon from the API response
-// and deserializes the data into a custom struct and returns it or an error if an unexpected failure occurs.
-func (pc *PokeClient) unmarshalPokemon(p []byte) (*models.PokemonResponse, error) {
-	pokeRes := &models.PokemonResponse{}
-	err := json.Unmarshal(p, pokeRes)
-	return pokeRes, err
-}
-
+// GetPokemon invokes the pokemon API to get a collection of pokemon.
 func (pc *PokeClient) GetPokemon(o, l string) (*models.PaginatedResponse, error) {
 	u, err := url.Parse(pc.baseUrl)
 	if err != nil {
